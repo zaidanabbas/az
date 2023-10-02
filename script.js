7782c68bca2f65e55ee0ab24bb180f34ac20f0b8
Abbas Zaidan <abbas.zaidan08@gmail.com>
2023-10-02 09:15:57 +0300

    updating

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function btn() {
         document.getElementById("contact").style.backgroundColor = 'black';
     }
 }
+
+let letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+// let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+document.getElementById("abbas").onmouseover = event => {
+  let iterations = 0;
+  const interval = setInterval(() => {
+    event.target.innerText = event.target.innerText.split("")
+      .map((letter, index) => {
+         
+         return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("")
+    if(iterations >= 9) clearInterval(interval);
+    
+    iterations += 1;
+  }, 50)
+         
+}
